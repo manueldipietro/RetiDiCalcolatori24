f1672b4973ddfe14b15480c1d683adaea160ae23
manueldipietro <78982527+manueldipietro@users.noreply.github.com>
2024-05-01 18:53:14 +0200

    Delete greddis directory

--- greddis/ADT/adt_utility.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include <error.h>
-
-void adt_utils_print_error(const char* file_name, int line, const char* function_name, const char* error_description);
--- greddis/utility.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include <error.h>
-#include <time.h>
-#include <unistd.h>
-
-void print_error(const char* file_name, int line, const char* function_name, const char* error_description);
